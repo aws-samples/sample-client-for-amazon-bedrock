994e1127f368aa67b5f1e342ba51d237bde2a87c
glay <glay.guo@foxmail.com>
2024-07-03 14:05:32 +0800

    Delete out directory

--- out/_next/static/UXMP5iFOCGBksb2dl6rOx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-a216d61e3fc034fb.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
