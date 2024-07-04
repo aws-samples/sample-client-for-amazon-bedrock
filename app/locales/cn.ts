90a7f3da9952ee7da1afd8e7a0125fbff462336b
AutoJunjie <38706868+AutoJunjie@users.noreply.github.com>
2024-07-04 12:11:14 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -7,7 +7,7 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized: isApp
-      ? "检测到无效 API Key，请前往[设置](/#/settings)页检查 API Key 是否配置正确。"
+      ? "认证信息不正确或为空，请前往[登录](/#/auth)页输入正确的AWS认证信息"
       : "认证信息不正确或为空，请前往[登录](/#/auth)页输入正确的AWS认证信息",
   },
   Auth: {
